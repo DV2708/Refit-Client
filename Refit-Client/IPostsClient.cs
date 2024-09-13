6b234d8c2c725570e8baf55d68992c3bd99aaf05
Dinesh Vasudevan <dineshvsvce@gmail.com>
2024-09-13 20:09:09 +0530

    Add project files.

---
+++ Refit-Client/IPostsClient.cs
@@ -0,0 +1,23 @@
+﻿using Refit;
+
+namespace Refit_Client
+{
+    public interface IPostsClient
+    {
+        [Get("/posts")]
+        Task<IEnumerable<Post>> GetAll();
+
+        [Get("/posts/{id}")]
+        Task<Post> GetPost(int id);
+
+        [Post("/posts")]
+        Task<Post> CreatePost([Body] Post post);
+
+        [Put("/posts/{id}")]
+        Task<Post> UpdatePost(int id, [Body] Post post);
+
+        [Delete("/posts/{id}")]
+        Task DeletePost(int id);
+    }
+}
+
---
+++ Refit-Client/Program.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Refit;
+using Refit_Client;
+
+using var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((_, services) =>
+    {
+        services
+            .AddRefitClient<IPostsClient>()
+            .ConfigureHttpClient(c => c.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/"));
+    }).Build();
+
+var postsClient = host.Services.GetRequiredService<IPostsClient>();
+
+// Get All Post
+Console.WriteLine(">> >> >> Get All Post >> >> >>");
+var posts = await postsClient.GetAll();
+
+foreach (var post in posts)
+{
+    Console.WriteLine(post.Title);
+}
+Console.WriteLine(">> >> >> Get All Post ended >> >> >>" + Environment.NewLine);
+
+// Create
+Console.WriteLine(">> >> >> Create a new post >> >> >>");
+var newPost = new Post
+{
+    Title = "New Post",
+    Body = "New Post",
+    UserId = 1
+};
+
+var newPostId = (await postsClient.CreatePost(newPost)).Id;
+Console.WriteLine($"Post with Id: {newPostId} created");
+Console.WriteLine(">> >> >> Post created >> >> >>" + Environment.NewLine);
+
+// Update
+Console.WriteLine(">> >> >> Update a post >> >> >>");
+var existingPost = await postsClient.GetPost(1);
+existingPost.Title = "New Title is given";
+var updatedPost = await postsClient.UpdatePost(existingPost.Id, existingPost);
+Console.WriteLine($"Post Title updated to {updatedPost.Title}");
+Console.WriteLine(">> >> >> Post Updated >> >> >>" + Environment.NewLine);
+
+// Delete
+Console.WriteLine(">> >> >> Delete a post >> >> >>");
+await postsClient.DeletePost(newPostId);
+Console.WriteLine(">> Post Deleted");
+
+Console.Read();
